285a02a3d3dd0755f9047ff6b3d3c06e4504efc8
Vitor Talaia <vitor.oliveira2@etec.sp.gov.br>
2015-12-27 19:32:28 -0200

    Create a user profile card

--- app/assets/javascripts/components/timeline-app.js.jsx
+++ app/assets/javascripts/components/timeline-app.js.jsx
@@ -11,6 +11,18 @@ var TimelineApp = React.createClass({
         </h3>
 
         <Searchbar onSearch={this.handleSearch} />
+
+        <section>
+          <UserProfile
+            name="Vitor Talaia"
+            username="vitortalaia"
+            description="Writing code at @Codeminer42; Formula One fan. Don't worry, I'm pushing like hell!"
+            tweets={24809}
+            following={1838}
+            followers={737}
+            image="https://pbs.twimg.com/profile_images/609558297537024000/wp8agzeN_200x200.jpg"
+            banner="https://pbs.twimg.com/profile_banners/56174169/1428202948" />
+        </section>
       </div>
     );
   }
