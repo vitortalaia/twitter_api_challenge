c958408a6d2ea698c5f413505e6ccfd7851eaba3
Vitor Talaia <vitor.oliveira2@etec.sp.gov.br>
2015-12-28 12:30:07 -0200

    Change timeline representation

--- app/representers/timeline_representer.rb
+++ app/representers/timeline_representer.rb
@@ -8,16 +8,8 @@ class TimelineRepresenter < Representable::Decorator
   property :text
   property :hashtags
   property :user_mentions
-  property :retweeted_status?
-  property :retweeted_status
-  property :quoted_status?
-  property :quoted_status
 
   property :user do
-    property :id
-    property :created_at
-    property :verified?, as: :verified
-    property :protected?, as: :protected
     property :name
     property :screen_name
     property :description
