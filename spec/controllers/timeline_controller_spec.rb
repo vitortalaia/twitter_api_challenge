8aac993660c503f582f5afca5b4414c4c8ea2b05
Vitor Talaia <vitor.oliveira2@etec.sp.gov.br>
2015-12-28 16:04:33 -0200

    Add missing tests do timeline controller spec

--- spec/controllers/timeline_controller_spec.rb
+++ spec/controllers/timeline_controller_spec.rb
@@ -21,5 +21,27 @@ describe TimelineController do
     it 'returns a JSON representation' do
       expect(subject.content_type).to eq Mime::JSON
     end
+
+    context "when the specified username doesn't exist" do
+      before do
+        allow(TimelineService).to receive(:perform)
+          .and_raise(Twitter::Error::NotFound)
+      end
+
+      it 'returns a 404 NOT FOUND status code' do
+        expect(subject).to be_not_found
+      end
+    end
+
+    context "when the specified username has a private profile" do
+      before do
+        allow(TimelineService).to receive(:perform)
+          .and_raise(Twitter::Error::Unauthorized)
+      end
+
+      it 'returns a 422 UNPROCESSABLE ENTITY status code' do
+        expect(subject.status).to eq 422
+      end
+    end
   end
 end
